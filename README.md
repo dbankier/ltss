bc9f4e5b9d6debb2eb2934e349353bdab0f9e74b
David Bankier <dbankier@hotmail.com>
2013-07-23 12:11:10 +1000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,142 @@
+# Less TSS
+
+An Alloy tss pre-processor inspired by less
+
+Not that it isn't less so less features won't necessarily work
+
+# Syntax
+
+## Variables
+
+```
+@color: #4D926F;
+
+#header {
+  color: @color;
+}
+Label {
+  color: @color;
+}
+```
+
+becomes
+
+```
+#header {
+  color: #4D926F;
+}
+Label {
+  color: #4D926F;
+}
+```
+
+## Mixins
+
+```
+.padding (@pad: '5dp') {
+  top: @pad,
+  bottom: @pad,
+  left: @pad,
+  right: @pad
+}
+
+#header {
+  .padding;
+}
+#footer {
+  .padding('10px');
+}
+```
+
+becomes
+
+```
+#header {
+  
+  top: '5dp',
+  bottom: '5dp',
+  left: '5dp',
+  right: '5dp';
+}
+#footer {
+  
+  top: '10px',
+  bottom: '10px',
+  left: '10px',
+  right: '10px';
+}
+
+```
+
+
+## CLI Usage
+
+
+```
+ltss [filename.ltss]
+```
+
+converted file will be return to stdout
+
+```
+ltss [filename.ltss] filename.tss
+```
+
+converted file will be written to the output file provided
+
+
+## Library Usage
+
+the following commands are available: `compileString(string, callback)`, `compileFile(filename, callback)`,
+`writeFile(source, target, callback)`
+
+## Extended Example
+
+```
+@variable1: 1;
+@test2  :  3
+
+.mixin1 (@arg1, @arg2) {
+  length: @arg1,
+  bredth: @arg2
+}
+
+.mixin2(@color: 'red') {
+  backgroundColor: @color
+}
+
+
+".class" : {
+  height: @variable1,
+  text: 'hello',
+  width: @test2,
+  .mixin2(),
+  .mixin1(20, 30)
+}
+
+".class2" : {
+  .mixin2("blue")
+}
+```
+
+becomes
+
+```
+".class" : {
+  height: 1,
+  text: 'hello',
+  width: 3,
+  backgroundColor: 'red',
+  length: 20,
+  bredth:  30
+}
+
+".class2" : {
+  backgroundColor: "blue"
+}
+```
+
+## TODO
+
+An alloy.jmk file to automate the process...
+
---
+++ ltss/ltss.js
@@ -0,0 +1,74 @@
+var fs = require("fs");
+
+var variables = {},
+    mixins = {};
+
+function extractVariables(source) {
+  //var regex = /^(@[a-zA-Z0-9]+)[ \t]*:[ \t]*(["']?.*?["']?)[ \t]*;?$/g
+  var regex = /(?:^|\n)(@\w+)[ \t]*:[ \t]*([^;\n]*);?/g
+  var match = regex.exec(source) 
+  while (match != null) {
+    variables[match[1]] = match[2];
+    match = regex.exec(source);
+  }
+  source = source.replace(regex, "");
+  return source;
+}
+
+function extractMixins(source) {
+  var regex = /(?:^|\n)([\w.#]*)[ \t]*\((.*?)\)[ \t]{([\s\S]*?)\n}/g
+  var match = regex.exec(source) 
+  while (match != null) {
+    mixins[match[1]] = { 
+      args: match[2].split(",").map(function(a) { 
+        return a.trim().split(":").map(function(b) {
+          return b.trim();
+        });
+      }), 
+      content: match[3]
+    };
+    match = regex.exec(source);
+  }
+  source = source.replace(regex, "");
+  return source;
+}
+
+function replaceMixins(source) {
+  var regex = /([\w.#]*)[ \t]*\((.*?)\)/g
+  var match = regex.exec(source) 
+  while (match != null) {
+    var mixin = mixins[match[1]];
+    var content = mixin.content;
+    var args = match[2].split(",");
+    var nonempty = match[2].trim().length > 0;
+    mixin.args.forEach(function(a,idx) {
+      content = content.replace(new RegExp(a[0], 'g'), idx>args.length -1 || !nonempty ? a[1] : args[idx]);
+    });
+    source = source.replace(match[0], content);
+    match = regex.exec(source);
+  }
+  return source;
+
+}
+exports.compileString = function(source,callback) {
+  source = extractVariables(source);
+  source = extractMixins(source);
+  for (var key in variables) {
+    source = source.replace(new RegExp(key, 'g'), variables[key]);
+  }
+  source = replaceMixins(source);
+  callback(null, source);
+};
+
+exports.compileFile = function(file, callback) {
+  fs.readFile(file,{encoding: 'utf8'},  function(err, data) {
+    exports.compileString(data,callback);
+  });
+}
+
+exports.writeFile = function(source, target, callback) {
+  exports.compileFile(source, function(err,data) {
+    fs.writeFileSync(target,data);
+    callback(err,target + " written");
+  });
+}
