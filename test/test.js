b63dd1819b0687ae01caba8fc5c6f9fea190d715
Ashton Williams <Ashton.Williams@philology.com.au>
2013-10-16 13:10:40 +1100

    nom test was giving false positives, using the mocha `done` callback seems to fix it.

--- test/test.js
+++ test/test.js
@@ -3,34 +3,37 @@ var ltss = require("../ltss/ltss"),
     assert = require("assert");
 
 describe("variables test", function() {
-  it("should parse", function() {
+  it("should parse", function(done) {
     ltss.compileFile('./test/variables.ltss',function(err,data) {
       assert.equal(data, fs.readFileSync('./test/variables.tss').toString());
+      done();
     });
   });
 });
 
 describe("mixins test", function() {
-  it("should parse", function() {
+  it("should parse", function(done) {
     ltss.compileFile('./test/mixin.ltss',function(err,data) {
       assert.equal(data, fs.readFileSync('./test/mixin.tss').toString());
+      done();
     });
   });
 });
 
 describe("include test", function() {
-  it("should parse", function() {
+  it("should parse", function(done) {
     ltss.compileFile('./test/include.ltss',function(err,data) {
       assert.equal(data, fs.readFileSync('./test/include.tss').toString());
+      done();
     });
   });
 });
 
-
 describe("combined test", function() {
-  it("should parse", function() {
+  it("should parse", function(done) {
     ltss.compileFile('./test/test.ltss',function(err,data) {
       assert.equal(data, fs.readFileSync('./test/test.tss').toString());
+      done();
     });
   });
 });
